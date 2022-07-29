8e566240290a2f55e9d7c34c8830c3d0ae991414
raphaella-rose <raphaella.rose1@gmail.com>
2022-07-29 10:40:14 +0100

    refactored spellChecker function to return the flagged incorrect word when it is spelt wrong

--- spellChecker.js
+++ spellChecker.js
@@ -1,14 +1,11 @@
 const spellChecker = (word) => {
-  if (word == 'thess') {
-    return '~thess~'
-  } else if (word == 'wrds') {
-    return '~wrds~'
-  } else if (word == 'aer') {
-    return '~aer~'
-  }
+
   const correctWords = ['these', 'words', 'are'];
   let correctedWord = correctWords.find(correctWord => correctWord == word);
 
+  if (correctedWord == undefined) {
+    correctedWord = `~${word}~`
+  }
   return correctedWord;
 }
 
