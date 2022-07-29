0e9b381b30109fbd072660afc8649a032c1b1a8e
raphaella-rose <raphaella.rose1@gmail.com>
2022-07-29 10:44:24 +0100

    refactored spellChecker argument name to be text

--- spellChecker.js
+++ spellChecker.js
@@ -1,12 +1,12 @@
-const spellChecker = (word) => {
-  if (word == 'these words') {
+const spellChecker = (text) => {
+  if (text == 'these words') {
     return 'these words'
   }
   const correctWords = ['these', 'words', 'are'];
-  let correctedWord = correctWords.find(correctWord => correctWord == word);
+  let correctedWord = correctWords.find(correctWord => correctWord == text);
 
   if (correctedWord == undefined) {
-    correctedWord = `~${word}~`
+    correctedWord = `~${text}~`
   }
   return correctedWord;
 }
