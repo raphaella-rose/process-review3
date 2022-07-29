26e4e8e08898208b62ab2f0ffee1e6f5d61f543d
raphaella-rose <raphaella.rose1@gmail.com>
2022-07-29 10:16:09 +0100

    spellChecker returns words when it is spelt correctly

--- spellChecker.js
+++ spellChecker.js
@@ -1,5 +1,10 @@
-const spellChecker = () => {
-  return 'these'
+const spellChecker = (word) => {
+  if (word == 'these') {
+    return 'these'
+  } else {
+    return 'words'
+  }
+
 }
 
 module.exports = spellChecker;
--- spellChecker.test.js
+++ spellChecker.test.js
@@ -1,8 +1,11 @@
 const spellChecker = require("./spellChecker");
 
 describe('spellChecker', () => {
-  it("returns the word inputted when it is spelt correctly", () => {
-
+  it("returns the word 'these' when it is spelt correctly", () => {
     expect(spellChecker('these')).toBe('these');
+  })
+
+  it("returns the word 'words' when it is spelt correctly", () => {
+    expect(spellChecker('words')).toBe('words');
   })
 })
